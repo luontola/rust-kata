4c7967fd2c59a74d4d962bc29eafe52a5bb4d385
Esko Luontola <esko@luontola.fi>
2024-01-08 21:20:19 +0200

    Start working on AOC 2017 day 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
     println!("Hello, world!");
 }
+
+// https://adventofcode.com/2017/day/1
+#[cfg(test)]
+mod aoc2017_day1_tests {
+    #[test]
+    fn test_evaluate_pair() {
+        assert_eq!(1, evaluate_pair(1, 1));
+        assert_eq!(0, evaluate_pair(1, 2));
+        assert_eq!(6, evaluate_pair(6, 6));
+    }
+
+    fn evaluate_pair(a: i32, b: i32) -> i32 {
+        if (a == b) {
+            a
+        } else {
+            0
+        }
+    }
+}
